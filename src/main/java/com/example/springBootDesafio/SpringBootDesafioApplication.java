29567c2214ec0cc78a5fc8668ff2057663aaa603
s.santos.pimenta <simoneperiodista3@gmail.com>
2020-12-29 15:17:23 -0300

    "Created Setup. Close #1"

---
+++ src/main/java/com/example/springBootDesafio/SpringBootDesafioApplication.java
@@ -0,0 +1,13 @@
+package com.example.springBootDesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDesafioApplication.class, args);
+	}
+
+}
